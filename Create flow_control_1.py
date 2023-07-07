505b69b1371bd7556ae623d72f5e539c4ec60833
FortuneAnefiok <135363953+FortuneAnefiok@users.noreply.github.com>
2023-07-07 14:35:02 +0100

    Update Create flow_control_1.py

--- Create flow_control_1.py
+++ Create flow_control_1.py
@@ -1,1 +1,13 @@
-
+# Assignment 3
+# Program to print divisibility of both numbers 3, 5 and (3&5)
+m = 0
+while m < 50:
+    m = m + 1
+    if m % 3 == 0 and m % 5 == 0:
+        print("Couple")
+    elif m % 5 == 0:
+        print("Girl")
+    elif m % 3 == 0:
+        print("Boy")
+    else:
+        print(m)
